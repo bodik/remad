16223c236d978236cd681eced5eb7837d3b7a057
root <root@ztook2.meta.zcu.cz>
2017-11-06 23:47:27 +0100

    directory refactoring

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for all in $(find "$(dirname $(readlink -f $0))/test" -type f -name '*sh'); do 
+for all in $(find "$(dirname $(readlink -f $0))/tests" -type f -name '*sh'); do 
 	echo "INFO: run test $all"
 	sh $all
 done
