a7134908d5cb8a03afb1e8bf7608e03f213710ea
root <root@ztook9.meta.zcu.cz>
2017-11-09 17:31:50 +0100

    proper mit kadmin rekeying support

--- tests/createkeytab.sh
+++ tests/createkeytab.sh
@@ -4,7 +4,7 @@
 
 KEYTAB="/tmp/test_remad_createkeytab_$$"
 for service in ftp pbs nfs; do
-	if [ -f /etc/krb5/kadm5.acl ]; then
+	if [ -f /etc/krb5kdc/kadm5.acl ]; then
 		kadmin.local delprinc $service/$(facter fqdn) 1>/dev/null 2>/dev/null
 	fi
 	if [ -f /etc/heimdal-kdc/kadmind.acl ]; then
@@ -14,13 +14,13 @@ done
 
 # create
 kinit -k -t /etc/krb5.keytab host/$(facter fqdn)
-remad --debug createkeytab --host $(facter fqdn) --services host ftp pbs nfs --outfile ${KEYTAB}
+remad --debug createkeytab --host $(facter fqdn) --services ftp pbs nfs --outfile ${KEYTAB}
 if [ $? -ne 0 ]; then
 	rreturn 1 "remad failed"
 fi
 
 # test
-for service in host ftp pbs nfs; do
+for service in ftp pbs nfs; do
 	kinit -k -t ${KEYTAB} ${service}/$(facter fqdn) || rreturn 1 "$0 kinit $service failed"
 done
 
--- tests/createkeytab_rekey.sh
+++ tests/createkeytab_rekey.sh
@@ -2,9 +2,29 @@
 
 . /puppet/metalib/bin/lib.sh
 
+getkvno() {
+	KEYTAB="$1"
+	PRINCIPAL="$2"
+
+	if [ -f /etc/krb5kdc/kadm5.acl ]; then
+		echo "$(klist -k ${KEYTAB} | grep ${service}/$(facter fqdn) | head -n1 | awk '{print $1}')"
+		return
+	fi
+
+	if [ -f /etc/heimdal-kdc/kadmind.acl ]; then
+		echo "$(ktutil -k ${KEYTAB} list | grep ${service}/$(facter fqdn) | head -n1 | awk '{print $1}')"
+		return
+	fi
+
+	echo "error"
+	return
+}
+
+
+
 KEYTAB="/tmp/test_remad_createkeytab_$$"
 for service in ftp pbs nfs; do
-	if [ -f /etc/krb5/kadm5.acl ]; then
+	if [ -f /etc/krb5kdc/kadm5.acl ]; then
 		kadmin.local delprinc $service/$(facter fqdn) 1>/dev/null 2>/dev/null
 	fi
 	if [ -f /etc/heimdal-kdc/kadmind.acl ]; then
@@ -26,7 +46,7 @@ for service in ftp pbs nfs; do
 done
 
 for service in ftp pbs nfs; do
-	KVNO=$(ktutil -k ${KEYTAB} list | grep ${service}/$(facter fqdn) | head -n1 | awk '{print $1}')
+	KVNO=$(getkvno ${KEYTAB} ${service}/$(facter fqdn))
 	if [ "$KVNO" != "1" ]; then
 		rreturn 1 "wrong upserted kvno"
 	fi
@@ -48,7 +68,7 @@ for service in ftp pbs nfs; do
 done
 
 for service in ftp pbs nfs; do
-	KVNO=$(ktutil -k ${KEYTAB} list | grep ${service}/$(facter fqdn) | head -n1 | awk '{print $1}')
+	KVNO=$(getkvno ${KEYTAB} ${service}/$(facter fqdn))
 	if [ "$KVNO" != "2" ]; then
 		rreturn 1 "wrong upserted kvno"
 	fi
