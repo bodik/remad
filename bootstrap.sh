15c3aba2ccaf26278020b3ab5edd37aa2b070e91
Radoslav Bodó <bodik@cesnet.cz>
2019-11-14 15:04:56 +0100

    repository placement update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 if [ ! -d /opt/remad ]; then
-	cd /opt
-	git clone https://rsyslog.metacentrum.cz/remad.git
+	git clone https://gitlab.meta.zcu.cz/bodik/remad.git /opt/remad
+	cd /opt/remad
+	git remote set-url origin --push bodik@gitlab.meta.zcu.cz:bodik/remad.git
 else
 	cd /opt/remad
-	git remote set-url origin https://rsyslog.metacentrum.cz/remad.git
 	git pull
 fi
-
-cd /opt/remad && git remote set-url origin bodik@rsyslog.metacentrum.cz:/data/remad.git
